f0a2f5540ecc3da2673388aa098204017018b4f1
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-10-18 14:38:45 +0300

    Sort import statements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import {MAIN_BACKGROUND_COLOR, MAIN_TEXT_COLOR} from "./consts";
-import Header from "./header";
 import CategoryList from "./categories";
+import Header from "./header";
 import React from "react";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
+import {MAIN_TEXT_COLOR} from "./consts";
 
 
 const theme = createTheme({
