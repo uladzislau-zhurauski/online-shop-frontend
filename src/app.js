fae5179e605379270e7b232485202c88274d6a1c
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2021-10-23 00:49:59 +0300

    Add background color

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import Header from "./header/header";
 import React from "react";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
-import {MAIN_TEXT_COLOR} from "./consts";
+import {MAIN_BACKGROUND_COLOR, MAIN_TEXT_COLOR} from "./consts";
 
 
 const theme = createTheme({
     palette: {
         primary: {
             main: MAIN_TEXT_COLOR,
+        },
+        secondary: {
+            main: MAIN_BACKGROUND_COLOR,
         }
     },
 });
--- src/consts.js
+++ src/consts.js
@@ -9,6 +9,7 @@ export const MASTERS_PHONE_NUMBER = '+375 44 550 90 59'
 export const MASTERS_EMAIL = 'kitaeska@gmail.com'
 export const NAVBAR_LINKS_NAMES = ["Главная", "Обо мне", "Оплата и Доставка", "Отзывы", "Контакты", "Оставить заявку"];
 export const MAIN_TEXT_COLOR = '#749F95';
+export const MAIN_BACKGROUND_COLOR = '#9DEDDA';
 
 
 export const AccountButton = () => (
