6777b77e8031317e7c6a8b4aeedacb85dcbc5153
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2022-06-01 19:05:57 +0300

    feat: create sign in page

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {Link as RouterLink} from "react-router-dom";
+import StandardButton from "../../styles/StandardButton";
+
+const SignIn = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}
+            >
+                {/*<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>*/}
+                {/*    <LockOutlinedIcon />*/}
+                {/*</Avatar>*/}
+                <Typography component="h1" variant="h5">
+                    Войти в аккаунт
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Адрес электронной почты"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Пароль"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Запомнить меня"
+                    />
+                    <StandardButton
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Войти
+                    </StandardButton>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Забыли пароль?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link component={RouterLink} to="/sign-up" variant="body2">
+                                {"Нет аккаунта? Зарегистрироваться"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
+}
+
+export default SignIn;
