47c8e04044ed72dd5a40b0dd83b1c1ec227e281f
Uladzislau Zhurauski <Uladzislau.Zhurauski@itechart-group.com>
2022-06-03 20:24:31 +0300

    feat: create RootDiv component

---
+++ src/styles/RootDiv/index.js
@@ -0,0 +1,10 @@
+import {styled} from "@mui/material/styles";
+
+
+const RootDiv = styled('div')`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
+
+export default RootDiv;
